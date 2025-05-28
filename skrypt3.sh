c9a9c59a14e57a55f982ef3160c60fe844a24a1a
„Mateusz” <dsw58476@student.dsw.edu.pl>
2025-05-28 22:06:26 +0200

    dodano opcje --init ktora pozwala klonowac repozytorium oraz ustawia PATH

--- skrypt3.sh
+++ skrypt3.sh
@@ -4,6 +4,7 @@ case "$1" in
     --date|-d)
         date
         ;;
+
     --logs|-l)
         count=100
         [[ "$2" =~ ^[0-9]+$ ]] && count=$2
@@ -11,12 +12,36 @@ case "$1" in
             echo -e "Plik: log$i.txt\nSkrypt: $0\nData: $(date)" > "log$i.txt"
         done
         ;;
+
+    --error|-e)
+        count=100
+        [[ "$2" =~ ^[0-9]+$ ]] && count=$2
+        mkdir -p errorx
+        for i in $(seq 1 $count); do
+            echo -e "Błąd numer $i\nSkrypt: $0\nData: $(date)" > "errorx/error$i.txt"
+        done
+        ;;
+
+    --init)
+        repo_url="https://github.com/dsw58476/learning.git"
+        target_dir="$(pwd)/learning"
+        git clone "$repo_url" "$target_dir"
+        if ! grep -q "$target_dir" ~/.bashrc; then
+            echo "export PATH=\$PATH:$target_dir" >> ~/.bashrc
+            echo "Dodano $target_dir do PATH w ~/.bashrc"
+        fi
+        echo "Repozytorium sklonowane do $target_dir"
+        ;;
+
     --help|-h)
         echo "Dostępne opcje:"
-        echo "--date, -d     - wyświetla datę"
-        echo "--logs [N], -l N - tworzy N (domyślnie 100) plików logx.txt"
-        echo "--help, -h     - wyświetla pomoc"
+        echo "--date, -d          - wyświetla datę"
+        echo "--logs [N], -l N    - tworzy N (domyślnie 100) plików logx.txt"
+        echo "--error [N], -e N   - tworzy N (domyślnie 100) plików errorx/errorX.txt"
+        echo "--init              - klonuje repo https://github.com/dsw58476/learning.git i dodaje je do PATH"
+        echo "--help, -h          - wyświetla tę pomoc"
         ;;
+
     *)
         echo "Nieznana opcja: $1"
         ;;
